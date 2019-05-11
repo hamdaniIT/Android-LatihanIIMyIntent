f5077f02d1663965dafab2bfe4c0ffc6e9b49206
hamdaniIT <HamdaniIT@github.com>
2019-05-11 09:10:43 +0800

    Menambahkan button pada activity_main.xml dan membuat activity MoveWithDataActivity.xml, Menerapkan intent

--- app/src/main/java/com/example/latihaniimyintent/MainActivity.java
+++ app/src/main/java/com/example/latihaniimyintent/MainActivity.java
@@ -7,14 +7,16 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnMoveActivity;
+    Button btnMoveActivity,btnMoveWithDataActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnMoveActivity=findViewById(R.id.btn_move_activity);
+        btnMoveWithDataActivity=findViewById(R.id.btn_move_activity_data);
 
         btnMoveActivity.setOnClickListener(this);
+        btnMoveWithDataActivity.setOnClickListener(this);
 
 
     }
@@ -25,6 +27,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_move_activity:
                 Intent moveIntent= new Intent(MainActivity.this,MoveActivity.class);
                 startActivity(moveIntent);
+                break;
+
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent= new Intent(MainActivity.this,MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME,"Hamdani");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,24);
+                startActivity(moveWithDataIntent);
                 break;
 
         }
---
+++ app/src/main/java/com/example/latihaniimyintent/MoveWithDataActivity.java
@@ -0,0 +1,25 @@
+package com.example.latihaniimyintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithDataActivity extends AppCompatActivity {
+    TextView tvDataReceived;
+    public static final String EXTRA_AGE="extra_age";
+    public static final String EXTRA_NAME="extra_name";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+        tvDataReceived=findViewById(R.id.tv_data_received);
+
+        String nama=  getIntent().getStringExtra(EXTRA_NAME);
+        int age= getIntent().getIntExtra(EXTRA_AGE,0);
+
+        String text="Nama : " + nama + ", \nYour Age : "+ age;
+        tvDataReceived.setText(text);
+
+
+    }
+}
