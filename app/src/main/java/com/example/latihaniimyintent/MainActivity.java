2c276bef6710d3067581fea235174279edf9ac52
hamdaniIT <HamdaniIT@github.com>
2019-05-11 19:14:19 +0800

    Menambahkan Butoon untuk implicit intent pada activity_main.xml dan source code pada MainActivity.java

--- app/src/main/java/com/example/latihaniimyintent/MainActivity.java
+++ app/src/main/java/com/example/latihaniimyintent/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.latihaniimyintent;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnMoveActivity,btnMoveWithDataActivity;
+    Button btnMoveActivity,btnMoveWithDataActivity,btnDialNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnMoveActivity=findViewById(R.id.btn_move_activity);
         btnMoveWithDataActivity=findViewById(R.id.btn_move_activity_data);
+        btnDialNumber=findViewById(R.id.btn_dial_number);
 
         btnMoveActivity.setOnClickListener(this);
         btnMoveWithDataActivity.setOnClickListener(this);
+        btnDialNumber.setOnClickListener(this);
 
 
     }
@@ -34,6 +37,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME,"Hamdani");
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,24);
                 startActivity(moveWithDataIntent);
+                break;
+            case R.id.btn_dial_number:
+                String phoneNumber="082350321212";
+                Intent dialPhoneIntent= new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhoneIntent);
                 break;
 
         }
